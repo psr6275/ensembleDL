3d86af72cc7ad5f749339b2f709b8914f169da13
saerom <psr6275>
2019-04-08 22:32:36 +0900

    add dropout part to Correction model in ensemble.py

--- models/ensemble.py
+++ models/ensemble.py
@@ -2,16 +2,21 @@ import torch.nn as nn
 
 
 class Correct(nn.Module):
-    def __init__(self,base_clf):
+    def __init__(self,base_clf,dropout_tf = False):
         super(Correct, self).__init__()
         self.num_features,self.features = self._get_conv_layers(base_clf)
         self.correctness = nn.Linear(self.num_features,1)
+        self.dropout_tf = dropout_tf
+        if dropout_tf:
+            self.dropout = nn.Dropout2d(0.5)
         self.prob = nn.Sigmoid()
 
     def forward(self,x):
         out = self.features(x)
         out = out.view(out.size(0),-1)
         out = self.correctness(out)
+        if self.dropout_tf:
+            out = self.dropout(out)
         out = self.prob(out)
         return out
 
